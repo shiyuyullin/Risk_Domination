f3b31eee78c91389d405ef7adf2f0a1c1e527cd0
Ian Lopez <43527915+ianlop@users.noreply.github.com>
2019-09-30 10:36:20 -0400

    Add files via upload

---
+++ part3/Dice.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdlib> //Rand and SRand functions for random numbers
+#include <ctime>
+#include "Dice.h"
+using namespace std;
+
+Dice::Dice(void) : value(0), TotalRolls(0), DiceValues{}, percentage{}{
+
+} //initialize variables for the class
+//define roll method
+
+ 
+void Dice::Roll(int n) {
+	 //generating random number from 0-5 and then add 1
+	for (int i = 0; i < n; i++) {
+	TotalRolls++;
+	//generate random number from 1-6
+		value = (rand() % 6) + 1;
+		DiceValues[i] = value;
+		cout << "Value for die " << (i+1)<<": "<< value << endl;
+		//store the dice values that have been rolled in an array for percentage
+		percentage[(value - 1)]++;//increments the value of the die array (percentage)
+	}
+}
+
+
+void Dice::Display(int rolls) {
+	for (int i = 0; i < rolls; i++)
+		cout << "Container value " << (i + 1) << ": " << DiceValues[i] << endl;
+	
+	for (int i = 0; i < 6; i++)
+		cout << "Overall for Roll value " << (i + 1) << ": " << percentage[i] << endl;
+
+}
+
+
+void Dice::Reset() {
+
+	TotalRolls = 0;
+}
+
+void Dice::percentages()
+{
+	for (int i = 0; i < 6; i++)
+		cout<<"Percentage for Dice Value "<< (i+1)<<": "<<(percentage[i] / TotalRolls)<<endl;
+}
---
+++ part3/DiceDriver.cpp
@@ -0,0 +1,7 @@
+#include <ostream>
+#include "Dice.h"
+#include <cstdlib> //Rand and SRand functions for random numbers
+#include <ctime>
+#include <iostream>
+using namespace std;
+
