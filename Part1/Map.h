7fa2d1d14e109087f162eef79fbc35d343075f2e
Ahobeychi <a.hobeychi@outlook.com>
2019-09-30 18:56:28 -0400

    part 1 plan

---
+++ Part1/Map.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+
+class Map {
+private:
+	int numberOfCountries;
+	int routes[][];
+
+
+public:
+	Map();
+
+	boolean Validate();
+	void Display();
+	
+	//private memeber fields
+};
---
+++ Part1/map.cpp
@@ -0,0 +1,61 @@
+#include <string>
+
+
+
+//WORK IN PROGRESS STILL A
+
+
+class Map {
+
+public:
+	
+	int map[][];
+	map <string, int> continentRequirements;
+
+	Map(int nbOfCountries,) {
+		numberOfCountries = nbOfCountries;
+		ma
+
+	}
+
+private:
+	int numberOfCountries;
+
+
+};
+
+
+class Route {
+
+};
+
+class Country {
+
+private:
+	int continent;
+	int armySize;
+	string name;
+	int owner;
+
+public:
+	
+	int getContinent() {
+		return continent;
+	}
+
+	int getArmySize() {
+		return armySize;
+	}
+
+	int getOwner() {
+		return owner;
+	}
+
+	string getName(){
+		return name;
+	}
+
+};
+
+
+
