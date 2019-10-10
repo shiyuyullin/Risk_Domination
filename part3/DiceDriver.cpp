e62545e764af9c820d119b1604b623c593d4e9c3
Ian Lopez <43527915+ianlop@users.noreply.github.com>
2019-10-10 11:48:38 -0400

    Update DiceDriver.cpp

--- part3/DiceDriver.cpp
+++ part3/DiceDriver.cpp
@@ -10,7 +10,7 @@ int main() {
 	//to generate diff values the application runs
 	//or else they stay the same
 	srand(static_cast<unsigned int> (time(0)));
-	Dice MyDice1;
+	Dice* MyDice1 = new Dice();
 	int rolls = 0;
 	while (rolls <= 0 || rolls > 3) {
 		cout << "How many times would you like to roll? \nNumbers (1-3): " << endl;
@@ -18,13 +18,12 @@ int main() {
 		if (rolls <= 0 || rolls > 3)
 			cout << "Invalid roll number.\n" << endl;
 	}
-	MyDice1.Roll(rolls);
-	//-------INSERT HERE LATER TO RESET ROLLS TO 0-----------
-	MyDice1.Display(rolls);
+	MyDice1->Roll(rolls);
+	MyDice1->Display(rolls);
 
 	cout << "Percentages of Dice Values (1-6) rolled\n";
-	MyDice1.percentages();
-	cout << "\nDICE 2 OBJECT\n";
+	MyDice1->percentages();
+	cout << "\nDICE 2 OBJECT with no pointers\n";
 	Dice MyDice2;
 	rolls = 0;
 	while (rolls <= 0 || rolls > 3) {
@@ -36,4 +35,6 @@ int main() {
 	MyDice2.Roll(rolls);
 	MyDice2.Display(rolls);
 	MyDice2.percentages();
+	delete MyDice1;
 }
+
