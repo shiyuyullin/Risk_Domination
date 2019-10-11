9be4d73c6c0195eb242faaf6a075e471ad6b01d7
Ian Lopez <43527915+ianlop@users.noreply.github.com>
2019-10-10 20:15:57 -0400

    Add files via upload
    A main driver i think we could use to have all of our code work at once

---
+++ MainDriver.cpp
@@ -0,0 +1,19 @@
+#include <ostream>
+#include "Dice.h"
+#include <iostream>
+#include "Player.h"
+using namespace std;
+
+int main() {
+	Dice test;
+	cout << "Dice------------"<<endl;
+	test.DiceMain();
+	cout << "Player------------"<<endl;
+	Player player1;
+	player1.RollDice(3);
+	player1.attack();
+	player1.foritfy();
+	player1.reinforce();
+	player1.DisplayRollInfo(3);
+	return 0;
+}
