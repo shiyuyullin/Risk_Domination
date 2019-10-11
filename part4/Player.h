00b606075c4a3739535e9868d19bcbaa132fcc74
Ian Lopez <43527915+ianlop@users.noreply.github.com>
2019-10-10 20:06:36 -0400

    Update Player.h

--- part4/Player.h
+++ part4/Player.h
@@ -4,22 +4,17 @@
 #include <ostream>
 #include <vector>
 class Player{
-protected:
-	Dice dice;//player dices
-
-	//Map class that will be implemented later and it'll have its countires the player owns
-	/*std::vector<Map> countries;
-	countries.reserve(20);
-	*/
-	
-	//there must be a static counter to keep track on how many countries there are. This information is
-	//needed to know how many cards there will be and how many spaces i can make in my array.
-	//Deck deck
+private:
+	Dice* dice;//player dices
+	//Countries needed
+	//Deck needed
 
 public:
 	Player();
+	~Player();
 	void reinforce();
 	void attack();
 	void foritfy();
-	void RollDice();
+	void RollDice(int n); //Test the dice obj for player
+	void DisplayRollInfo(int n); // These two here are to be generally put together
 };
