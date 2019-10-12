c388a1fe560a258f171ca6caed2204c8e9f4eeab
Ian Lopez <43527915+ianlop@users.noreply.github.com>
2019-10-12 19:24:31 -0400

    Update PlayerDriver.cpp

--- part4/PlayerDriver.cpp
+++ part4/PlayerDriver.cpp
@@ -6,22 +6,15 @@ using namespace std;
 
 int main() {
 	Player player1;
-
-	player1.RollDice();
-	
-	/*
-	Here we will implement later a test on implementing the countries and
-	deck (cards)
-	I propose a vector of countries with a certain set of reserved
-	memory slots (for each country)
-
-	the card count should reflect the deck count but i do not think that
-	will apply to the player class but the deck class instead
-	.h file contains my variable names for that
-	*/
-
+	player1.RollDice(5);
 	player1.attack();
 	player1.foritfy();
 	player1.reinforce();
+	int NumberOfCountries = 42;
+	Deck deck(NumberOfCountries);
+	player1.hand->addToHand(deck.draw());
+	player1.hand->addToHand(deck.draw());
+	player1.hand->addToHand(deck.draw());
+	player1.hand->showHand();
 
 }
