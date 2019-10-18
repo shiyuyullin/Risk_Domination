c86071117ecb0066def1874328df39496766a9ab
Ian Lopez <43527915+ianlop@users.noreply.github.com>
2019-10-18 11:45:18 -0400

    Update DiceDriver.cpp

--- part3/DiceDriver.cpp
+++ part3/DiceDriver.cpp
@@ -12,10 +12,10 @@ int main() {
 	srand(static_cast<unsigned int> (time(0)));
 	Dice* MyDice1 = new Dice();
 	int rolls = 0;
-	while (rolls <= 0 || rolls > 100) {
-		cout << "How many times would you like to roll? \nNumbers (1-100): " << endl;
+	while (rolls <= 0 || rolls > 3) {
+		cout << "How many times would you like to roll? \nNumbers (1-3): " << endl;
 		cin >> rolls;
-		if (rolls <= 0 || rolls > 100)
+		if (rolls <= 0 || rolls > 3)
 			cout << "Invalid roll number.\n" << endl;
 	}
 	MyDice1->Roll(rolls);
@@ -23,18 +23,7 @@ int main() {
 
 	cout << "Percentages of Dice Values (1-6) rolled\n";
 	MyDice1->percentages();
-	cout << "\nDICE 2 OBJECT with no pointers\n";
-	Dice MyDice2;
-	rolls = 0;
-	while (rolls <= 0 || rolls > 3) {
-		cout << "How many times would you like to roll? \nNumbers (1-3): " << endl;
-		cin >> rolls;
-		if (rolls <= 0 || rolls > 3)
-			cout << "Invalid roll number.\n" << endl;
-	}
-	MyDice2.Roll(rolls);
-	MyDice2.Display(rolls);
-	MyDice2.percentages();
 	delete MyDice1;
 }
+
 
