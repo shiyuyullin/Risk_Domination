cc3c111fe3a20879c56abd7a052dc3ba762d4f8d
Shiyu Lin <45950481+shiyuyullin@users.noreply.github.com>
2019-10-18 15:09:09 -0400

    Add files via upload

---
+++ Assignment_1/Part3/Dice.h
@@ -0,0 +1,16 @@
+#pragma once
+class Dice {
+private:
+	int *valueptr, *TotalRollsptr;
+	int DiceValues[3];
+	double percentage[6];
+
+
+public:
+	Dice();
+	~Dice();
+	void Roll(int n);
+	void Display(int rolls);
+	void Reset();
+	void percentages();
+};
---
+++ Assignment_1/Part3/DiceDriver.cpp
@@ -0,0 +1,29 @@
+#include <ostream>
+#include "Dice.h"
+#include <cstdlib> //Rand and SRand functions for random numbers
+#include <ctime>
+#include <iostream>
+using namespace std;
+
+int main() {
+	//seed the random number generator
+	//to generate diff values the application runs
+	//or else they stay the same
+	srand(static_cast<unsigned int> (time(0)));
+	Dice* MyDice1 = new Dice();
+	int rolls = 0;
+	while (rolls <= 0 || rolls > 3) {
+		cout << "How many times would you like to roll? \nNumbers (1-3): " << endl;
+		cin >> rolls;
+		if (rolls <= 0 || rolls > 3)
+			cout << "Invalid roll number.\n" << endl;
+	}
+	MyDice1->Roll(rolls);
+	MyDice1->Display(rolls);
+
+	cout << "Percentages of Dice Values (1-6) rolled\n";
+	MyDice1->percentages();
+	delete MyDice1;
+}
+
+
