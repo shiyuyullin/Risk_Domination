6a60b673cac5cba7640462e4ef1de6a877d961eb
Ian Lopez <43527915+ianlop@users.noreply.github.com>
2019-10-18 11:44:04 -0400

    Update Dice.h

--- part3/Dice.h
+++ part3/Dice.h
@@ -1,19 +1,16 @@
 #pragma once
 class Dice {
 private:
-	int value, TotalRolls;
 	int *valueptr, *TotalRollsptr;
-	int DiceValues[100];
+	int DiceValues[3];
 	double percentage[6];
-	int* DVptr=DiceValues;
-	double* Pptr = percentage;
+
 
 public:
 	Dice();
+	~Dice();
 	void Roll(int n);
 	void Display(int rolls);
-	void Reset(); //private fields set back to 0
-	//private memeber fields
+	void Reset();
 	void percentages();
-	void DiceMain();
 };
