1376734460fd871b00c81148193a8e5dad64d7e5
Ian Lopez <43527915+ianlop@users.noreply.github.com>
2019-10-18 11:43:19 -0400

    Update Dice.cpp

--- part3/Dice.cpp
+++ part3/Dice.cpp
@@ -4,11 +4,22 @@
 #include "Dice.h"
 using namespace std;
  //initialize variables for the class
-Dice::Dice(void) : value(0), TotalRolls(0),DiceValues{}, percentage{}{
-	TotalRollsptr = &TotalRolls;
-	valueptr = &value;
-	DVptr = DiceValues;
-	Pptr = percentage;
+
+
+Dice::Dice()
+{
+	valueptr = new int(0);
+	TotalRollsptr = new int(0);
+	*DiceValues = {};
+	*percentage = {};
+}
+
+Dice::~Dice()
+{
+	delete valueptr;
+	valueptr = NULL;
+	delete TotalRollsptr;
+	TotalRollsptr = NULL;
 }
 
 //define roll method 
@@ -17,8 +28,8 @@ void Dice::Roll(int n) {
 	for (int i = 0; i < n; i++) {
 	(*TotalRollsptr)++;
 	//generate random number from 1-6
-		value = (rand() % 6) + 1;
-		DiceValues[i] = value;
+		*valueptr = (rand() % 6) + 1;
+		DiceValues[i] = *valueptr;
 		cout << "Value for die " << (i+1)<<": "<< *valueptr << endl;
 		//store the dice values that have been rolled in an array for percentage
 		percentage[(*valueptr - 1)]++;//increments the value of the die array (percentage)
@@ -28,11 +39,11 @@ void Dice::Roll(int n) {
 
 void Dice::Display(int rolls) {
 	for (int i = 0; i < rolls; i++)
-		cout << "Container value " << (i + 1) << ": " << *(DVptr + i) << endl;
+		cout << "Container value " << (i + 1) << ": " << DiceValues[i] << endl;
 		//cout << "Container value " << (i + 1) << ": " << DiceValues[i] << endl;
 	
 	for (int i = 0; i < 6; i++)
-		cout << "Overall for Roll value " << (i + 1) << ": " << *(Pptr + i) << endl;
+		cout << "Overall for Roll value " << (i + 1) << ": " << percentage[i] << endl;
 		//cout << "Overall for Roll value " << (i + 1) << ": " << percentage[i] << endl;
 
 }
@@ -40,47 +51,13 @@ void Dice::Display(int rolls) {
 
 void Dice::Reset() {
 
-	TotalRolls = 0;
+	*TotalRollsptr = 0;
 }
 
 void Dice::percentages()
 {
 	for (int i = 0; i < 6; i++)
-		cout<<"Percentage for Dice Value "<< (i+1)<<": "<<(percentage[i] / TotalRolls)<<endl;
-}
-
-void Dice::DiceMain() //Testing the "main function" for Dice
-{
-	//seed the random number generator
-//to generate diff values the application runs
-//or else they stay the same
-	srand(static_cast<unsigned int> (time(0)));
-	Dice* MyDice1 = new Dice();
-	int rolls = 0;
-	while (rolls <= 0 || rolls > 100) {
-		cout << "How many times would you like to roll? \nNumbers (1-3): " << endl;
-		cin >> rolls;
-		if (rolls <= 0 || rolls > 100)
-			cout << "Invalid roll number.\n" << endl;
-	}
-	MyDice1->Roll(rolls);
-	MyDice1->Display(rolls);
-
-	cout << "Percentages of Dice Values (1-6) rolled\n";
-	MyDice1->percentages();
-	cout << "\nDICE 2 OBJECT with no pointers\n";
-	Dice MyDice2;
-	rolls = 0;
-	while (rolls <= 0 || rolls > 3) {
-		cout << "How many times would you like to roll? \nNumbers (1-3): " << endl;
-		cin >> rolls;
-		if (rolls <= 0 || rolls > 3)
-			cout << "Invalid roll number.\n" << endl;
-	}
-	MyDice2.Roll(rolls);
-	MyDice2.Display(rolls);
-	MyDice2.percentages();
-	delete MyDice1;
+		cout<<"Percentage for Dice Value "<< (i+1)<<": "<<(percentage[i] / *TotalRollsptr)<<endl;
 }
 
 
