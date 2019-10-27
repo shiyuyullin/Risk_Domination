3dcd8728ff1f85f8b2fb08e1b49c9b2354663af7
Shiyu Lin <45950481+shiyuyullin@users.noreply.github.com>
2019-10-27 11:39:46 -0400

    Add files via upload

--- Assignment_1/Part2/MapLoaderDriver.cpp
+++ Assignment_1/Part2/MapLoaderDriver.cpp
@@ -3,29 +3,16 @@
 #include <string>
 #include <sstream>
 #include "MapLoader.h"
+#include "D:\Visual Studio\WorkSpace\Map\Map.h"//The Map.h file's path
 
 int main() {
 
-	map loadedMap;
+	Map* loadedMap;
 
 	loadedMap = Maploader::loadingMap("ameroki");
-	cout << loadedMap.countries[0]->getSerialNumber() << endl;
-	cout << loadedMap.countries[0]->getCountryName() << endl;
-	cout << loadedMap.countries[0]->getContinent() << endl;
-	loadedMap.countries[0]->displayBorders();//checking the contents
-
-	loadedMap = Maploader::loadingMap("invMap");//invalid map
-
-	map anotherMap1 = Maploader::loadingMap("luca");
-	cout << anotherMap1.continents[0]->getSerialNum() << endl;
-	cout << anotherMap1.continents[0]->getName() << endl;//checking the contents
-	cout << anotherMap1.continents[0]->getReward() << endl;
-
-
-	map anotherMap2 = Maploader::loadingMap("eurasien");
-	map anotherMap3 = Maploader::loadingMap("roman_empire");
-
-
+	cout << loadedMap->arrOfCountries[0]->getCountryNumber() << endl;
+	cout << loadedMap->arrOfCountries[0]->getCountryName() << endl;
+	cout << loadedMap->arrOfCountries[0]->getContinent() << endl;
 
 
 
