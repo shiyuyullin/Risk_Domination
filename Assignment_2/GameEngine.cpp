bfd51c67edc4adfb892bd42f0579665c683b12f5
Ian Lopez <43527915+ianlop@users.noreply.github.com>
2019-10-29 16:26:10 -0400

    Create GameEngine.cpp

---
+++ Assignment_2/GameEngine.cpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "Player.h"
+class GameEngine {
+private:
+	int turn_decider;
+	std::vector<Player*> turns;//an array that keeps trak who goes when(1,2,3,...N)
+	std::vector<Player> players;//an array of player objects that will play per game
+
+public:
+	void order_of_play();
+	void map_assign();
+	const int number_of_armies_given(int AmtOfPlayers);
+
+};
