49040010241e953246c5106e341dd9afe8330f7a
Ian Lopez <43527915+ianlop@users.noreply.github.com>
2019-10-29 17:00:38 -0400

    Update GameEngine.cpp

--- Assignment_2/GameEngine.cpp
+++ Assignment_2/GameEngine.cpp
@@ -2,13 +2,13 @@
 #include "Player.h"
 class GameEngine {
 private:
-	int turn_decider;
-	std::vector<Player*> turns;//an array that keeps trak who goes when(1,2,3,...N)
-	std::vector<Player> players;//an array of player objects that will play per game
+	int turn_decider;//Ian
+	std::vector<Player*> turns;//an array that keeps trak who goes when(1,2,3,...N)//Ian
+	
 
 public:
-	void order_of_play();
-	void map_assign();
-	const int number_of_armies_given(int AmtOfPlayers);
+	void order_of_play();//Ian
+	void map_assign();//Ian
+	const int number_of_armies_given(int AmtOfPlayers);//Ian
 
 };
