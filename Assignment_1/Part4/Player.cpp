f0af1a494432cdc82d2f81412cc6f4af8abfe8bb
Shiyu Lin <45950481+shiyuyullin@users.noreply.github.com>
2019-10-29 12:15:06 -0400

    Add files via upload

--- Assignment_1/Part4/Player.cpp
+++ Assignment_1/Part4/Player.cpp
@@ -5,7 +5,8 @@ Player::Player()
 {
 	dice = new Dice();
 	hand = new Hand();
-	//Countries needed here
+	indexOfCountryOwned = new int[70];//Serial number of country 1,2,3..... corresponding to country[0], country[1],.....
+	numberOfCountryOwned = new int(0);
 }
 
 Player::~Player()
@@ -13,6 +14,8 @@ Player::~Player()
 
 	delete dice;
 	dice = NULL;
+	delete[] indexOfCountryOwned;
+	delete numberOfCountryOwned;
 }
 
 void Player::reinforce()
@@ -22,7 +25,48 @@ void Player::reinforce()
 
 void Player::attack()
 {
-	cout << "This method will attack" << endl;
+	Map* a = new Map();//A fake map that will be used temporarily, will be the map that has been selected.
+	int state = 0;
+	while (state != 1) {
+		cout << "Welcome to the attack phase" << endl;
+		cout << "Please choose the following options: 1. attack 2. not attack." << endl;
+		cout << "if you want to attack, enter 1, if not enter 2: ";
+		cin >> state;
+		if (state == 1) {//enter the attack phase loop
+
+
+
+
+
+
+
+		}
+		else if (state == 2) {//return from method attack
+			return;
+		}
+		else {//Invalid input
+			cout << "Invalid input, please try again." << endl;
+
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
  
 void Player::foritfy()
@@ -40,6 +84,9 @@ void Player::DisplayRollInfo(int n) //Displays: Roll values stored in the array 
 {
 	dice->Display(n);
 	dice->percentages();
+}
+void Player::incrementNumOfCountry() {
+	numberOfCountryOwned++;
 }
 
 
