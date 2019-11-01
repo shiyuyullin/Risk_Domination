db9d6829f152bd9e527f36cdbc3a1fadae9e5f65
Shiyu Lin <45950481+shiyuyullin@users.noreply.github.com>
2019-11-01 15:03:00 -0400

    Add files via upload

--- Assignment_1/Part4/Player.h
+++ Assignment_1/Part4/Player.h
@@ -1,28 +1,35 @@
 #pragma once
 #include <iostream>
 #include "Dice.h"
+#include "Cards.h"
 #include <ostream>
 #include <vector>
-#include "Part1/Map.h"
-
+class Map;
 
 class Player{
 private:
 	Dice* dice;//player dices
-	//Countries needed
+	int* indexOfCountryOwned;
 	Hand* hand;
+	int* numberOfCountryOwned;
 	int* playerId;
+	int* armies;
 
 public:
 	Player();
-	Player(int id,int armyCount);
+	Player(int id, int armyCount);
 	~Player();
+	int getNumOwnedCountry();
+	int findIndex(int serialNum);//Finding the index of a country's serial number in the "indexOfCountryOwned"
+	int getPlayerID();
+	void setPlayerID(int id);
 	void reinforce();
 	void attack();
 	void foritfy();
 	void RollDice(int n); //Test the dice obj for player
 	void DisplayRollInfo(int n); // These two here are to be generally put together
-	void setPlayerId(int id);
-	int getPlayerId();
-	bool operator==( const Player &otherPlayer ) const ;
+	void incrementNumOfCountry();
+	void decrementNumOfCountry();
+	void setIndexOfCountry(int serialNum);
+	void reOrganize();//if a player losses one of its country, reorganize his "indexOfCountryOwned"
 };
