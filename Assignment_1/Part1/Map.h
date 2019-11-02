9d22bab1ec6cdbf09948b8e1486726dd2307100d
Shiyu Lin <45950481+shiyuyullin@users.noreply.github.com>
2019-11-01 20:57:54 -0400

    Add files via upload

--- Assignment_1/Part1/Map.h
+++ Assignment_1/Part1/Map.h
@@ -2,51 +2,48 @@
 #include <map>
 #include <string>
 #include "D:\Visual Studio\WorkSpace\Player\Player.h"
-
 using namespace std;
 
 class Country
 {
-	string *countryName;
-	int *continent;
-	int *nbOfArmies;
-	Player *owner;
-	int *countryNumber;
-	int *borders;		  //Boarders of a country. A pointer points to an array of ints
-	int *numberOfBorders; //A counter for number of borders, may be used for iteration throw the borders[]
+	string* countryName;
+	int* continent;
+	int* nbOfArmies;
+	Player* owner;
+	int* countryNumber;
+	int* borders;//Boarders of a country. A pointer points to an array of ints
+	int* numberOfBorders;//A counter for number of borders, may be used for iteration throw the borders[]
 public:
 	Country();
 	Country(string n);
 	Country(string n, int countNb, int cont, int own);
-	Country(const Country &obj);
+	Country(const Country& obj);
 	~Country();
 	int getCountryNumber();
 	int getContinent();
 	int getNbOfArmies();
 	string getCountryName();
 	int getNumOfBorders();
-	int *getBorders();
-	Player *getOwner();
+	int* getBorders();
+	Player* getOwner();
 	void setCountryNumber(int n);
 	void setContinent(int otherContinent);
 	void setCountryName(string otherName);
 	void setArmyNumber(int armNb);
 	void setBorders(int index, int t);
 	void setNumOfBorders(int t);
-	void setOwner(Player *p);
+	void setOwner(Player* p);
 };
 
-class Continent
-{
-	int *serialNumber;
-	string *name;
-	int *rewards;
-	int *numOfCountries;
-	Country *listOfCountries;
 
+class Continent {
+	int* serialNumber;
+	string* name;
+	int* rewards;
+	int* numOfCountries;
 public:
 	Continent();
-	Continent(Continent &temp);
+	Continent(Continent& temp);
 	void setserialNumber(int t);
 	void setName(string t);
 	void setRewards(int t);
@@ -55,41 +52,41 @@ public:
 	string getName();
 	int getReward();
 	int getNumOfCountries();
-	void setContinentCountry(int );
 	~Continent();
 };
 
-class Map
-{
 
-	Country *arrOfCountries;	//Maploader will return you a Map which the arrOfCountries are all country objects, you may use them for your functions
-	Continent *arrOfContinents; //arrOfContinents are all continents object
-	int *numberOfContinents;
-	int *numberOfCountries;
-	int **mapOfCountries;
-	createAdjacencyMatrix();
 
+
+class Map {
+
+	Country* arrOfCountries[70];//Maploader will return you a Map which the arrOfCountries are all country objects, you may use them for your functions
+	Continent* arrOfContinents[15];//arrOfContinents are all continents object 
+	int* numberOfContinents;
+	int* numberOfCountries;
+	int** mapOfCountries;
+	std::map <std::string, int>* continentRequirements;
+	Country* listOfCountries;
 public:
 	Map();
 	Map(int nbOfCountries);
-	Map(string CountryNames[], int **adjacencyMatrix);
-	Map(int **adjacencyMatrix, Country *Countries, int nbOfCountries);
+	Map(string CountryNames[], int** adjacencyMatrix);
+	Map(int** adjacencyMatrix, Country* Countries, int nbOfCountries);
 	~Map();
-	void setContinent(int index, Continent *tempContinent); //setter for arrOfContinent
-	void setCountry(int index, Country *tempCountry);		//setter for arrOfCountry
-	void setborder(int index, int indexBorders, int num);   //setter for country's borders
-	void setNumberOfCountries(int numberOfCountries);		//setter to set number of countries
-	void setNumberOfContinents(int numberOfContinents);		//setter to set number of continents
+	void setContinent(int index, Continent* tempContinent);//setter for arrOfContinent
+	void setCountry(int index, Country* tempCountry);//setter for arrOfCountry
+	void setborder(int index, int indexBorders, int num);//setter for country's borders
+	void setNumberOfCountries(int numberOfCountries);//setter to set number of countries
+	void setNumberOfContinents(int numberOfContinents);//setter to set number of continents
 	bool validateMap(int continentStart, int continentEnd);
 	void printMap();
-	int **getMapOfCountries();
+	int** getMapOfCountries();
 	int getNumOfCountries();
 	int getNumOfContinents();
-	Country *getCountry(int index);
-	Continent *getContinent(int index);
-	void addEdge(int start, int destination);
-	void setContinentSizesAndMembers();
-
+	Country* getCountry(int index);
+	Continent* getContinent(int index);
+	bool addEdge(int start, int destination);
+	
 };
 
 void mapTestFunction();
