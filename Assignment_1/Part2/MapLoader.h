d37728e0a5ca86495b1a4c12e9eabefde9482670
Shiyu Lin <45950481+shiyuyullin@users.noreply.github.com>
2019-11-02 02:47:00 -0400

    Modifications

--- Assignment_1/Part2/MapLoader.h
+++ Assignment_1/Part2/MapLoader.h
@@ -1,7 +1,7 @@
 #pragma once
 #include<string>
 #include<iostream>
-#include "D:\Visual Studio\WorkSpace\Map\Map.h"//The Map.h file's path
+#include "Map.h"//The Map.h file's path
 using namespace std;
 
 class Maploader {
