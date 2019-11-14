f4d987cd8101c120665cf6ba7eb1efd9967fd837
Ian Lopez <43527915+ianlop@users.noreply.github.com>
2019-11-14 12:05:56 -0500

    Update Viewer.h

--- Viewer.h
+++ Viewer.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "GameObservers.h"
 #include "GameEngine.h"
-
+/*
+PhaseObserver, an observer of this program. They 
+are simple classes that display relevant information
+on what is going in game, mainly after the player has finished their
+attack,fortify, or reinforcement.
+Ian Lopez
+*/
 class PhaseObserver : public Observer {
-	/**
-	 * Concrete observer that views what is going on with the player's actions on 
-	 the game
-	 */
 
 private:
 	GameEngine* engine;
@@ -14,9 +16,9 @@ public:
 	PhaseObserver();
 	~PhaseObserver();
 	PhaseObserver(GameEngine* en);
-	void Update(int n);
-	void display(int n);
-	void displayReinforceInfo();
-	void displayAttackInfo();
-	void displayFortifyInfo();
+	void Update(int player_num,int action,string msg);
+	void display(int player_num, int action, string msg);
+	void displayReinforceInfo(int a1,string msg);//1
+	void displayAttackInfo(int a2,string msg);//2
+	void displayFortifyInfo(int a3,string msg);//3
 };
