717ab7c13c8441f42183a85f66d5b4de537a7983
Ian Lopez <43527915+ianlop@users.noreply.github.com>
2019-11-14 12:07:10 -0500

    Comments added

--- GameObservers.cpp
+++ GameObservers.cpp
@@ -1,5 +1,8 @@
 #include "GameObservers.h"
-
+/*
+The classes down here will be of no use for this assignment
+Ian Lopez
+*/
 Observer::~Observer()
 {
 }
@@ -9,7 +12,14 @@ Observer::Observer()
 }
 
 
-
+/*
+The subject defintions however are of high importance
+as these methods instantiate the list of observers
+adds and removes observers
+And finally, notifies the observers what happened in
+the current pahse
+Ian Lopez
+*/
 Subject::Subject() {
 	_observers = new list<Observer*>;
 }
@@ -22,8 +32,8 @@ void Subject::Attach(Observer* o) {
 void Subject::Detach(Observer* o) {
 	_observers->remove(o);
 };
-void Subject::Notify(int n) {
+void Subject::Notify(int player_num,int action,string msg) {
 	list<Observer*>::iterator i = _observers->begin();
 	for (; i != _observers->end(); ++i)
-		(*i)->Update(n);
+		(*i)->Update(player_num,action,msg);
 };
