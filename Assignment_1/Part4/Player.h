fc0b4ceb5219322c4bba40d7b29fa826254b47d1
Shiyu Lin <45950481+shiyuyullin@users.noreply.github.com>
2019-11-18 10:23:46 -0500

    Add files via upload

--- Assignment_1/Part4/Player.h
+++ Assignment_1/Part4/Player.h
@@ -1,7 +1,8 @@
-#pragma once
+﻿#pragma once
 #include <iostream>
 #include "D:\Visual Studio\WorkSpace\Dice\Dice.h"
 #include "D:\Visual Studio\WorkSpace\Deck\Cards.h"
+#include "D:\Visual Studio\WorkSpace\PlayerStrategies\PlayerStrategies.h"
 #include <ostream>
 #include <vector>
 #include <algorithm>
@@ -17,7 +18,8 @@ private:
 	int* numberOfCountryOwned;
 	int* playerId;
 	int* armies;
-	int* actionDoneHere;
+	Strategy* playerStrategy;
+
 
 public:
 	//Constructor
@@ -31,28 +33,23 @@ public:
 	int getPlayerId();
 	int getArmies();
 	int getSerialAt(int index);
-	Hand* getHand();
-	int getActionDoneHere();
-	
 	//Setters
 	void setPlayerId(int id);
-	void setIndexOfCountry(int serialNum);//just pass in the countries serial number
-	
-	//player actions
 	void reinforce();
 	void attack();
 	void foritfy();
-	
-	//dice actions
 	void RollDice(int n); //Test the dice obj for player
 	void DisplayRollInfo(int n); // These two here are to be generally put together
-	
 	void incrementNumOfCountry();
 	void decrementNumOfCountry();
-	
+	void setIndexOfCountry(int serialNum);//just pass in the countries serial number
 	void removeIndex(int index);
+	//setStrategy will set the player to perform a specific behavior(human, aggressive, benevolent)
+	void setStrategy(Strategy* newStrategy);
 	//A2P2 Ian start up phase function
 	void placeArmy();
-	
+	Hand* getHand();
+	Dice* getDice();
+
 };
 
