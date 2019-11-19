4c55cd93ea4d28b4840367fd895d00c3db317520
Shiyu Lin <45950481+shiyuyullin@users.noreply.github.com>
2019-11-18 20:44:25 -0500

    Add files via upload

--- Assignment_3/Part1/PlayerStrategies.h
+++ Assignment_3/Part1/PlayerStrategies.h
@@ -8,6 +8,10 @@ class GameEngine;
 class Dice;
 class Hand;
 
+//The strategy design pattern gives the ability to change player behaviors
+//for the same functions in the run time. 
+
+//The super class for all different player stratefies
 class Strategy {
 protected:
 	int* actionsdonehere = new int(0);
@@ -18,18 +22,27 @@ public:
 	const inline int getAction() { return *actionsdonehere; };
 };
 
+//The functions for human players require user interaction to make decisions
 class humanPlayer : public Strategy {
 	void Reinforce(Player* p);
 	void Attack(Player* p);
 	void Fortify(Player* p);
 };
 
+//The aggressive player(computer player) who focus on attack.
+// 1.It always reinforce its strongest country
+// 2.It always use that country to attack, until it cannot attack with that country anymore
+// 3.Fortifies in order to aggregate its armies into one country
 class aggressivePlayer : public Strategy {
 	void Reinforce(Player* p);
 	void Attack(Player* p);
 	void Fortify(Player* p);
 };
 
+//The benevolent player(computer player) who focus on protecting its weak countries
+// 1.reinforce to its weakest country(reinforce to country with smallest number of armies)
+// 2.never attack
+// 3.fortify in order to move armies to weaker countries(balance armies among countries)
 class beneloventPlayer : public Strategy {
 	void Reinforce(Player* p);
 	void Attack(Player* p);
---
+++ Assignment_3/Part1/PlayerStrategiesDriver.cpp
@@ -0,0 +1,59 @@
+﻿#include "PlayerStrategies.h"
+#include "D:\Visual Studio\WorkSpace\Player\Player.h"
+#include "D:\Visual Studio\WorkSpace\Map\Map.h"
+#include "D:\Visual Studio\WorkSpace\GameEngine\GameEngine.h" 
+#include "D:\Visual Studio\WorkSpace\Dice\Dice.h"
+#include "D:\Visual Studio\WorkSpace\Deck\Cards.h"
+#include "D:\Visual Studio\WorkSpace\a1_MapLoader\MapLoader.h"
+#include <iostream>
+
+
+int main() {
+	GameEngine* p = new GameEngine(2);
+	Map* gameMap = Maploader::loadingMap("ameroki.txt");
+	p->setMap(gameMap);
+	Player* a = new Player();
+	a->setStrategy(new aggressivePlayer);
+	Player* b = new Player();
+	Country* temp = new Country();
+
+	for (int i = 0; i < gameMap->getNumOfCountries(); i++) {
+		if (i % 2 == 0) {
+			temp = gameMap->getCountry(i);
+			temp->setArmyNumber(5);
+			temp->setOwner(a);
+			a->setIndexOfCountry(temp->getCountryNumber());
+			a->incrementNumOfCountry();
+		}
+		else {
+			temp = gameMap->getCountry(i);
+			temp->setArmyNumber(5);
+			temp->setOwner(b);
+			b->setIndexOfCountry(temp->getCountryNumber());
+			b->incrementNumOfCountry();
+		}
+	}
+	a->reinforce();
+	a->attack();
+	a->foritfy();
+	Country* tempC;
+	for (int i = 0; i < a->getNumOwnedCountry(); i++) {
+		tempC = gameMap->getCountry(a->getSerialAt(i) - 1);
+		cout << tempC->getCountryNumber() << " " << tempC->getCountryName() << " " << tempC->getNbOfArmies() << endl;
+	}
+	cout << endl;
+	cout << endl;
+
+	a->setStrategy(new beneloventPlayer);
+	a->reinforce();
+	a->attack();
+	a->foritfy();
+	for (int i = 0; i < a->getNumOwnedCountry(); i++) {
+		tempC = gameMap->getCountry(a->getSerialAt(i) - 1);
+		cout << tempC->getCountryNumber() << " " << tempC->getCountryName() << " " << tempC->getNbOfArmies() << endl;
+	}
+
+
+
+	return 0;
+}
